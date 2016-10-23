cb04e699de988b875ee020812f018f4fa4f29b3b
EliottTheCoder <eliott.henry94@gmail.com>
2016-10-23 14:10:46 +0200

    Update LastVersion.py

--- LastVersion.py
+++ LastVersion.py
@@ -41,12 +41,24 @@ def PrendreContenuFichier(fichierARecuperer, mode):
 			
 def CreationAttaque():
 	""" Créé une attaque selon les entrées de l'admin """
+	DEGATS_MAX = 200
 	print('Créez vos attaques !')
 	print('Choisissez un nom : ')
 	nom = input('> ') 
-	print('\nChoisissez les dégâts de cette attaque : ')
-	degats = input('> ')
-		
+	degats = -100
+	while degats <= 0 or degats > DEGATS_MAX:
+		try:
+			print('\nChoisissez les dégâts de cette attaque : ')
+			degats = input('> ')
+			degats = int(degats)
+			if degats <= 0:
+				print("Les dégâts de l'attaque doivent être positifs.")
+			if degats > DEGATS_MAX:
+				print("Les dégâts de l'attaque doivent être inférieurs à DEGATS_MAX, donc : ", DEGATS_MAX)
+		except ValueError:
+			print("Vous devez entrez un nombre.")
+			degats = -100
+	
 	AttaqueCree = Attaque(nom, degats)
 	enregistrerAttaque(AttaqueCree)
 
@@ -61,16 +73,56 @@ def enregistrerAttaque(Attaque):
 
 def CreationPokemon():	
 	""" Créé un pokemon selon les entrées de l'admin """
+	
+	PV_MAX = 300
+	VITESSE_MAX = 100
+	FORCE_MAX = 100
+	
 	print('Vous avez toujours rêvé de créer vos propres pokémons ? Vous pouvez enfin le faire !')
 	print('Choisissez un nom : ')
 	nom = input('> ') 
-	print('\nChoisissez le nb de pt de vie du pokemon : ')
-	pt_vie = input('> ')
-	print('\nChoisissez la vitesse du pokemon : ')
-	vitesse = input('> ')
-	print('\nChoisissez la force du pokemon : ')
-	force = input('> ')
-	print('\n')
+	
+	pt_vie = -100
+	while pt_vie <= 0 or pt_vie > PV_MAX:
+		try:
+			print('\nChoisissez le nombre de points de vie du pokemon : ')
+			pt_vie = input('> ')
+			pt_vie = int(pt_vie)
+			if pt_vie <= 0:
+				print("Le nombre de points de vie doit être supérieur à 0")
+			if pt_vie > PV_MAX:
+				print("Le nombre de points de vie doit être inférieur au PV_MAX, donc : ", PV_MAX)
+		except ValueError:
+			print("Vous devez entrer un nombre.")
+			pt_vie = -100
+	
+	vitesse = -100
+	while vitesse <= 0 or vitesse > VITESSE_MAX:
+		try:
+			print('\nChoisissez la vitesse du pokemon : ')
+			vitesse = input('> ')
+			vitesse = int(vitesse)
+			if vitesse <= 0:
+				print("Le nombre de points de vitesse doit être positif.")
+			if vitesse > VITESSE_MAX:
+				print("Le nombre de points de vitesse doit être inférieur a VITESSE_MAX, donc : ", VITESSE_MAX)
+		except ValueError:
+			print("Vous devez entrer un nombre")
+			vitesse = -100
+	
+	force = -100
+	while force <= 0 or force > FORCE_MAX:
+		try:
+			print('\nChoisissez la force du pokemon : ')
+			force = input('> ')
+			force = int(force)
+			if force <= 0:
+				print("Le nombre de points de force doit être positif.")
+			if force > FORCE_MAX:
+				print("Le nombre de points de force doit être inférieur a FORCE_MAX, donc : ", FORCE_MAX)
+		except ValueError:
+			print("Vous devez entrer un nombre")	
+			force = -100
 	
 	attaques = ChoisirAttaque() # Renvoie une liste de 4 objets Attaques
 	
